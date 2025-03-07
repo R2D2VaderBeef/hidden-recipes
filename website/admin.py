d07ff5aef971e49bf70d2755f1efcb628905e8d6
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2025-03-07 19:39:47 +0000

    feat: Register like and comment with admin

--- website/admin.py
+++ website/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from website.models import UserProfile
+from website.models import UserProfile, Tag, Recipe, Like, Comment
 
-from website.models import Tag, Recipe
-from website.models import UserProfile
-
+admin.site.register(UserProfile)
 admin.site.register(Tag)
 admin.site.register(Recipe)
-admin.site.register(UserProfile)
+admin.site.register(Like)
+admin.site.register(Comment)
