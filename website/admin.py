a7978874696570e80126a90eb4a5df3c7fc56c7a
Fatima kamran <2917308K@student.gla.ac.uk>
2025-03-07 13:35:56 +0000

    added admin register in admin.py

--- website/admin.py
+++ website/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from website.models import UserProfile
+
+from website.models import Tag, Recipe
+from website.models import UserProfile
+
+admin.site.register(Tag)
+admin.site.register(Recipe)
+admin.site.register(UserProfile)
