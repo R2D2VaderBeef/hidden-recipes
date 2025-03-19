9f26d6ea673fa2661b89b0cd1d54573957bb1136
Fatima kamran <2917308K@student.gla.ac.uk>
2025-03-19 14:18:37 +0000

    made comment section, users can add and delete comments

--- website/models.py
+++ website/models.py
@@ -38,4 +38,8 @@ class Comment(models.Model):
     text = models.CharField(max_length=255)
     poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments")
-    picture = models.ImageField(upload_to='comment_images', blank=True)
+
+
+
+    def __str__(self):
+        return self.text
