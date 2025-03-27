6e58a0bd6cd44745ed08cec0e58d191c757b06aa
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2025-03-27 15:25:27 +0000

    fix: Delete the omnipresent repeated imports

--- website/views.py
+++ website/views.py
@@ -88,12 +88,6 @@ def user_logout(request):
     logout(request)
     return redirect(reverse('website:home'))
 
-# website/views.py
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.models import User
-from django.core.paginator import Paginator
-from .models import Recipe
-
 def profile(request, username):
     user_obj = get_object_or_404(User, username=username)
     is_own_profile = request.user == user_obj
